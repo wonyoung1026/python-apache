9cc730c04763688cca1ab97646709ff13fdc2e39
Won <wonyoung1026@hotmail.com>
2019-12-11 03:16:27 +0900

    .

--- python-apache/python-apache.py
+++ python-apache/python-apache.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # Load environment variables
 #   > For security purpose 
 # -------------------------------------------------
-CONFIG_PATH = "config/.env"
+CONFIG_PATH = "config/env"
 
 def load_env():
     load_dotenv(dotenv_path=CONFIG_PATH)
